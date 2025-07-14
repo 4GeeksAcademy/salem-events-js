58a6153a8735d470de9768dd8ea712f140a3e381
salemmohdmohd <salim.farouq@gmail.com>
2025-07-14 14:36:01 -0500

    event

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
-import "bootstrap";
-import "./style.css";
-
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+const form = document.querySelector(".needs-validation");
+const errorAlert = document.getElementById("errorAlert");
+const submitButton = document.getElementById("submitButton");
+// Hide the error alert initially
+errorAlert.classList.add("d-none");
+submitButton.addEventListener("click", (event) => {
+  if (!form.checkValidity()) {
+    console.log("Form is invalid, showing error alert");
+    event.preventDefault();
+    event.stopPropagation();
+    errorAlert.classList.remove("d-none");
+  } else {
+    console.log("Form is valid, submitting");
+    errorAlert.classList.add("d-none");
+  }
+  form.classList.add("was-validated");
+});
